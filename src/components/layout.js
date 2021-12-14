72f2d505e5b638172c9e3efb78e1b7273bc5fc20
Chris Peters <bildepunkt@gmail.com>
2021-12-13 20:53:44 -0800

    add prettier workspace settings

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,8 +33,8 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       > */}
-        <main>{children}</main>
-        {/* <footer
+      <main>{children}</main>
+      {/* <footer
           style={{
             marginTop: `2rem`,
           }}

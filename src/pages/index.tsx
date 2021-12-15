76caa682993ad892f0c65232d18cd16b46efac08
Chris Peters <bildepunkt@gmail.com>
2021-12-14 19:33:13 -0800

    add logo and bg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,33 +5,21 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import "../styles/index.scss"
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <StaticImage
-      src="../images/logo.png"
-      quality={95}
-      layout={"fullWidth"}
-      formats={["auto", "webp", "avif"]}
-      alt="logo badge"
-    />
-    {/* <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p> */}
+
+    <div className="bg" />
+    <div className="logo">
+      <StaticImage
+        src="../images/logo.png"
+        quality={95}
+        alt="logo badge"
+        placeholder="blurred"
+      />
+    </div>
   </Layout>
 )
 
